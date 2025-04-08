b7c82af3dcb3d877edf61a8e0a56f6c87c9887b3
Gerardo Gonzalez Tulian <gerardo.gonzaleztulian@gmail.com>
2025-04-08 20:17:52 -0300

    Clean CajaFuerte.java

--- src/main/java/ar/edu/unahur/obj2/tdd/CajaFuerte.java
+++ src/main/java/ar/edu/unahur/obj2/tdd/CajaFuerte.java
@@ -1,25 +1,5 @@
 package ar.edu.unahur.obj2.tdd;
 
 public class CajaFuerte {
-    private Boolean estadoApertura;
-    private Integer codigoCorrecto;
 
-    public CajaFuerte() {
-        this.estadoApertura = Boolean.TRUE;
-    }
-
-    public boolean estaAbierta() {
-        return this.estadoApertura;
-    }
-
-    public void cerrar(Integer codigoDeApertura) {
-        this.estadoApertura = Boolean.FALSE;
-        this.codigoCorrecto = codigoDeApertura;
-    }
-
-    public void abrir(Integer codigoDeApertura) {
-        if (this.codigoCorrecto.equals(codigoDeApertura)) {
-            this.estadoApertura = Boolean.TRUE;
-        }
-    }
 }
